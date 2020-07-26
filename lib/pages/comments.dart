9384cddaf24111b51c23e703151d3a867fff1638
ArafatRohan93 <rohanarafat86@gmail.com>
2020-07-27 02:32:04 +0600

    Realtime Commenting

--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,20 +1,145 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+
+import 'home.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class Comments extends StatefulWidget {
+
+  final String postId;
+  final String postOwnerId;
+  final String postMediaUrl;
+
+  Comments({
+    this.postId,
+    this.postMediaUrl,
+    this.postOwnerId
+  });
+
   @override
-  CommentsState createState() => CommentsState();
+  CommentsState createState() => CommentsState(
+   postId:  this.postId,
+   postMediaUrl: this.postMediaUrl,
+   postOwnerId: this.postOwnerId
+  );
 }
 
 class CommentsState extends State<Comments> {
+
+  TextEditingController commentController = TextEditingController();
+
+    final String postId;
+  final String postOwnerId;
+  final String postMediaUrl;
+
+  CommentsState({
+    this.postId,
+    this.postMediaUrl,
+    this.postOwnerId
+  });
+
+  buildComments(){
+    return StreamBuilder(
+      stream: commentsRef
+      .document(postId)
+      .collection("comments")
+      .orderBy("timestamp", descending: false)
+      .snapshots(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return circularProgress();
+        }
+
+        List<Comment> comments =[];
+        snapshot.data.documents.forEach((doc){
+          comments.add(Comment.fromDocument(doc));
+        });
+        return ListView(
+          children: comments,
+        );
+
+      },
+    );
+  }
+
+  addComment(){
+    commentsRef
+    .document(postId)
+    .collection("comments")
+    .add({
+      "username" : currentUser.username,
+      "comment" : commentController.text,
+      "timestamp": timestamp,
+      "avatarUrl" : currentUser.photoUrl,
+      "userId" : currentUser.id,
+    });
+    commentController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Comments');
+    return Scaffold(
+      appBar: header(context,titleText: "Comments"),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: buildComments(),
+          ),
+          Divider(),
+          ListTile(
+            title: TextFormField(
+              controller: commentController,
+              decoration: InputDecoration(
+                labelText: "Write a comment...",
+              ),
+            ),
+            trailing: OutlineButton(
+              onPressed: () => addComment(),
+              borderSide: BorderSide.none,
+              child: Text("Post"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
 class Comment extends StatelessWidget {
+final String username;
+final String userId;
+final String avatarUrl;
+final String comment;
+final Timestamp timestamp;
+
+Comment({this.username,this.avatarUrl,this.comment,this.timestamp,this.userId});
+
+factory Comment.fromDocument(DocumentSnapshot doc){
+  return Comment(
+    username: doc['username'],
+    userId: doc['userId'],
+    avatarUrl: doc['avatarUrl'],
+    comment: doc['comment'],
+    timestamp: doc['timestamp'],
+  );
+}
+
   @override
   Widget build(BuildContext context) {
-    return Text('Comment');
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(comment),
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(avatarUrl),
+          ),
+          subtitle: Text(timeago.format(timestamp.toDate())),
+        ),
+        Divider(),
+      ],
+    );
   }
 }
--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -5,6 +5,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/comments.dart';
 import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/custom_image.dart';
 import 'package:fluttershare/widgets/progress.dart';
@@ -201,7 +202,12 @@ class _PostState extends State<Post> {
             ),
             Padding(padding: EdgeInsets.only(right: 20.0)),
             GestureDetector(
-              onTap: () => print("show comments"),
+              onTap: () => showComments(
+                context,
+                postId: postId,
+                ownerId: ownerId,
+                mediaUrl: mediaUrl
+              ),
               child: Icon(
                 Icons.chat,
                 size: 28.0,
@@ -259,4 +265,14 @@ class _PostState extends State<Post> {
       ],
     );
   }
+}
+
+showComments(BuildContext context, {String postId,  String ownerId, String mediaUrl }){
+Navigator.push(context, MaterialPageRoute(builder: (context){
+  return Comments(
+    postId: postId,
+    postOwnerId: ownerId,
+    postMediaUrl: mediaUrl,
+  );
+}));
 }
