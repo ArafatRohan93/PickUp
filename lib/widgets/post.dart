7f67bc45be7273de0cdeed4ef4407f5cbe439672
ArafatRohan93 <rohanarafat86@gmail.com>
2020-07-26 17:33:47 +0600

    Liking post with animation

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animator/animator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -35,17 +38,17 @@ class Post extends StatefulWidget {
     );
   }
 
-  int getLikeCount(likes){
+  int getLikeCount(likes) {
     //if no likes, return 0
-    if(likes == null){
+    if (likes == null) {
       return 0;
     }
     int count = 0;
 
     //if key is explicitly set to true, add a like
-    likes.values.forEach((val){
-      if(val == true){
-        count+=1;
+    likes.values.forEach((val) {
+      if (val == true) {
+        count += 1;
       }
     });
     return count;
@@ -53,149 +56,200 @@ class Post extends StatefulWidget {
 
   @override
   _PostState createState() => _PostState(
-     postId: this.postId,
-     ownerId: this.ownerId,
-     username:this.username,
-     location:  this.location,
-     description: this.description,
-     mediaUrl:  this.mediaUrl,
-     likes:  this.likes,
-     likesCount : getLikeCount(this.likes)
-  );
+      postId: this.postId,
+      ownerId: this.ownerId,
+      username: this.username,
+      location: this.location,
+      description: this.description,
+      mediaUrl: this.mediaUrl,
+      likes: this.likes,
+      likesCount: getLikeCount(this.likes));
 }
 
 class _PostState extends State<Post> {
-   final String postId;
+  final String currentUserId = currentUser?.id;
+  final String postId;
   final String ownerId;
   final String username;
   final String location;
   final String description;
   final String mediaUrl;
+  bool showHeart = false;
   int likesCount;
   Map likes;
+  bool isLiked;
 
-   _PostState(
-      {
-      this.postId,
+  _PostState(
+      {this.postId,
       this.username,
       this.description,
       this.likes,
       this.location,
       this.mediaUrl,
       this.ownerId,
-      this.likesCount
-      });
+      this.likesCount});
 
-      buildPostHeader(){
-        return FutureBuilder(
-          future: userRef.document(ownerId).get(),
-          builder: (context, snapshot){
-            if(!snapshot.hasData){
-              return circularProgress();
-            }
-            User user = User.fromDocument(snapshot.data);
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
-                backgroundColor: Colors.grey,
+  buildPostHeader() {
+    return FutureBuilder(
+      future: userRef.document(ownerId).get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        User user = User.fromDocument(snapshot.data);
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+            backgroundColor: Colors.grey,
+          ),
+          title: GestureDetector(
+            onTap: () => print('Showing Profile'),
+            child: Text(
+              user.username,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
-              title: GestureDetector(
-                onTap: ()=> print('Showing Profile'),
-                child: Text(
-                  user.username,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              subtitle: Text(location),
-              trailing: IconButton(
-                icon: Icon(Icons.more_vert),
-                onPressed: () => print('Deleting Post'),
-              ),
-            );
-          },
-        );
-      }
-
-      buildPostImage(){
-        return GestureDetector(
-          onDoubleTap: () => print('liking post'),
-          child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              cachedNetworkImage(mediaUrl),
-            ],
+            ),
+          ),
+          subtitle: Text(location),
+          trailing: IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () => print('Deleting Post'),
           ),
         );
-      }
+      },
+    );
+  }
 
-      buildPostFooter(){
-        return Column(
+  handleLikePost() {
+    bool _isLiked = likes[currentUserId] == true;
+
+    if (_isLiked) {
+      postsRef
+          .document(ownerId)
+          .collection("userPosts")
+          .document(postId)
+          .updateData({'likes.$currentUserId': false});
+      setState(() {
+        likesCount -= 1;
+        isLiked = false;
+        likes[currentUserId] = false;
+      });
+    } else if (!_isLiked) {
+      postsRef
+          .document(ownerId)
+          .collection("userPosts")
+          .document(postId)
+          .updateData({'likes.$currentUserId': true});
+      setState(() {
+        likesCount += 1;
+        isLiked = true;
+        showHeart = true;
+        likes[currentUserId] = true;
+      });
+      Timer(Duration(milliseconds: 500), () {
+        setState(() {
+          showHeart = false;
+        });
+      });
+    }
+  }
+
+  buildPostImage() {
+    return GestureDetector(
+      onDoubleTap: () => handleLikePost(),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          cachedNetworkImage(mediaUrl),
+          showHeart
+              ? Animator(
+                  duration: Duration(milliseconds: 300),
+                  tween: Tween(begin: 0.8, end: 1.4),
+                  curve: Curves.elasticOut,
+                  cycles: 0,
+                  builder: (anim) => Transform.scale(
+                    scale: anim.value,
+                    child: Icon(
+                      Icons.favorite,
+                      size: 80.0,
+                      color: Colors.red,
+                    ),
+                  ),
+                )
+              : Text(""),
+        ],
+      ),
+    );
+  }
+
+  buildPostFooter() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.only(top: 40.0, left: 20.0)),
-                  GestureDetector(
-                    onTap:  () => print("liking post"),
-                    child: Icon(
-                      Icons.favorite_border,
-                      size: 28.0,
-                      color: Colors.pink,
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(right: 20.0)),
-                  GestureDetector(
-                    onTap:  () => print("show comments"),
-                    child: Icon(
-                      Icons.chat,
-                      size: 28.0,
-                      color: Colors.blue[900],
-                    ),
-                  )
-                ],
+            Padding(padding: EdgeInsets.only(top: 40.0, left: 20.0)),
+            GestureDetector(
+              onTap: () => handleLikePost(),
+              child: Icon(
+                isLiked ? Icons.favorite : Icons.favorite_border,
+                size: 28.0,
+                color: Colors.pink,
               ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "$likesCount likes",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
+            ),
+            Padding(padding: EdgeInsets.only(right: 20.0)),
+            GestureDetector(
+              onTap: () => print("show comments"),
+              child: Icon(
+                Icons.chat,
+                size: 28.0,
+                color: Colors.blue[900],
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "$username",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text("$description"),
-                  ),
-                ],
-              ),
+            )
           ],
-        );
-      }
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$likesCount likes",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$username",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text("$description"),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    print("-------------Post state building-----------------");
+    isLiked = (likes[currentUserId] == true);
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
